be5350e83e2861fabfdbe002ce601ef23109baba
Artem <LD31D@github.com>
2019-12-26 16:23:30 +0200

    Update db.py

--- db.py
+++ db.py
@@ -1,3 +1,21 @@
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import create_engine
 
 engine = create_engine('sqlite:///db.sqlite3', echo=True)
+Base = declarative_base()
+
+
+class Music(Base):
+    __tablename__ = 'music'
+
+    id = Column(Integer, primary_key=True)
+    autor = Column(String)
+    name = Column(String)
+    len = Column(String)
+
+    def __str__(self):
+        return self.name
+
+
+Base.metadata.create_all(engine)
